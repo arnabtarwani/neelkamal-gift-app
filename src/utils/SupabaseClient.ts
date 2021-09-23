e356802e31be4d313444279a38e608b91552699d
Arnab Tarwani <arnab.tarwani@gmail.com>
2021-09-23 16:47:23 +0100

    App V1 Ready

--- src/utils/SupabaseClient.ts
+++ src/utils/SupabaseClient.ts
@@ -2,9 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 import { NEELKAMAL_APP_1_APP_URL, NEELKAMAL_APP_1_DB_KEY, NEELKAMAL_APP_2_APP_URL, NEELKAMAL_APP_2_DB_KEY } from "@env"
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = NEELKAMAL_APP_2_APP_URL 
+const supabaseUrl = NEELKAMAL_APP_1_APP_URL 
 
-const supabaseAnonKey = NEELKAMAL_APP_2_DB_KEY
+const supabaseAnonKey = NEELKAMAL_APP_1_DB_KEY
 
 const options = {
     localStorage: AsyncStorage as any,
